8cf69b25f91c5bae4908cb2ff6c75cdc02b9d21e
Žan Pevec <pevecyan@outlook.com>
2018-10-23 13:26:25 +0200

    Small fix

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ function defaultInterface () {
     var net = networks[names[i]]
     for (var j = 0; j < net.length; j++) {
       var iface = net[j]
-      if (iface.family === 'IPv4' && !iface.internal) return '0.0.0.0'
+      if (iface.family === 'IPv4' && !iface.internal) return iface.address
     }
   }
 
